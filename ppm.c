c4af709083b0371b299d8d1702513be73bd0117e
Thindex <thinudex100@gmail.com>
2017-02-12 00:53:53 -0700

    added comments to ppm.c

--- ppm.c
+++ ppm.c
@@ -1,3 +1,13 @@
+// Implementation of ppm.h
+// Handles the creation and editing of ppm files for other programs
+
+
+// desc: creates a ppm file with required header
+// args:
+//      name: name of ppm file
+//      width, height: dimensions of image
+//      max: max integer value for any RGB elements written
+// return: handle for created ppm file
 FILE* create_ppm(char* name, int width, int height, int max) {
     FILE* out = fopen(name, "w");
     fprintf(out, "P3\n");
@@ -9,7 +19,10 @@ FILE* create_ppm(char* name, int width, int height, int max) {
     
 }
 
-
+// desc: writes a pixel to a ppm file
+// args:
+//      r, g, b: RGB values for the pixel
+//      file:    file handle for the pm file
 void write_pixel(int r, int g, int b, FILE* file) {
     fprintf(file, "%d %d %d\n", r, g, b);
     
