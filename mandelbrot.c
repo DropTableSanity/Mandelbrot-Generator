e622acf2ea17017b8acb7a1307abb7fc61d23bbf
Thindex <thinudex100@gmail.com>
2017-02-12 00:49:09 -0700

    Added comments and pretties up mandelbrot.c

--- mandelbrot.c
+++ mandelbrot.c
@@ -3,13 +3,20 @@
 #include <stdio.h>
 #include "ppm.h"
 
-
+// desc: iterates through given coordinates and checks for divergence in the Mandelbrot set
+// args:
+//      x, y: x/y-coordinate
+//      magnitude: max amount of iterations allowed
+// return: int value for amount of iterations done before divergence. Will be magnitude if
+//         the coordinates converge
 int compute(double x, double y, int magnitude) {
     double complex a, b;
+    // set up starting number
     a = x + (y * I);
     b = 0;
     int iterations = 0;
     
+    // iterate through set, if b ever goes over 2, it diverges
     while(cabs(b) < 2 && iterations < magnitude) {
         b = (b * b) + a;
         iterations++;
@@ -22,35 +29,31 @@ int compute(double x, double y, int magnitude) {
 
 
 int main(int argc, char* argv) {
-    long double delta = .001;
-    long double i = 0;    
-    long double xStart = -3;
-    long double xEnd = 3;
-    long double yStart = -3;
-    long double yEnd = 3;
-    int max = 20;
-    FILE* output = create_ppm("output.ppm", 6000, 6000, max);
+    long double delta = .001;   // difference in each xy pair
+    long double xStart = -3;    // starting x coordinate
+    long double xEnd = 3;       // enidng x coordinate
+    long double yStart = -3;    // staring y coordinate
+    long double yEnd = 3;       // ending y coordinate
+    int max = 20;               // max rgb digit to be written to the output file
     
+    FILE* output = create_ppm("output.ppm", 6000, 6000, max);  // get file handle for output ppm
     
+    // nested loops for moving through xy coordinates
+    // moves from max y value to min y value, iterating via delta
     for(long double y = yEnd; y >= yStart; y -= delta) {
-        i++;
         
+        // moves from min x value to max x value, iterating via delta
         for(long double x = xStart; x <= xEnd; x += delta) {
-            int temp = compute(x, y, 0xFFFFFF);
-            write_pixel((temp >> 16) & 0xFF, (temp >> 8) & 0xFF, (temp) & 0xFF, output);  
+            int temp = compute(x, y, 0xFFFFFF);                                             // get amount of iterations before divergence
+            
+            // take the temp value and mask the bits to get the RGB value
+            // 0xFF FF FF
+            //   R  G  B
+            write_pixel((temp >> 16) & 0xFF, (temp >> 8) & 0xFF, (temp) & 0xFF, output);
 
         }
         
-        printf("%Lf\n", y);
-        
-        /*
-        if(i / 6000 > .1) {
-            printf("=");
-            fflush(stdout);
-            i = 0;
-            
-        }
-        */
+        printf("%Lf\n", y);  // print finished y value. Icky debug stuff
         
     }
 
