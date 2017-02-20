39a211af1a6c173d245d7eadea15e05c9b494f46
Thindex <thinudex100@gmail.com>
2017-02-20 11:12:35 -0700

    Added args struct and check_args()

--- mandelbrot.c
+++ mandelbrot.c
@@ -3,6 +3,32 @@
 #include <stdio.h>
 #include "ppm.h"
 
+
+// struct that holds all argument values for sanity's sake
+struct args {
+    long double delta, xStart, xEnd, yStart, yEnd, max;
+
+};
+
+struct args args;
+
+// desc: checks program arguments to make sure they're legal, and places them in the args struct
+// args:
+//      argc, argv: argument values from main()
+// return: int value. Either 0 for successful parsing, or -1 for unsuccessful parsing
+int check_args(int argc, char** argv) {
+    if(argc != 7) return -1;
+
+    sscanf(argv[1], "%Lf", &args.xStart);   // starting X digit   
+    sscanf(argv[2], "%Lf", &args.xEnd);     // ending X digit
+    sscanf(argv[3], "%Lf", &args.yStart);   // starting Y digit
+    sscanf(argv[4], "%Lf", &args.yEnd);     // ending Y digit
+    sscanf(argv[5], "%Lf", &args.delta);    // difference in each XY pair
+    sscanf(argv[6], "%Lf", &args.max);      // max RGB value
+
+}
+
+
 // desc: iterates through given coordinates and checks for divergence in the Mandelbrot set
 // args:
 //      x, y:      x/y-coordinate
@@ -28,15 +54,15 @@ int compute(double x, double y, int magnitude) {
 }
 
 
-int main(int argc, char* argv) {
+int main(int argc, char** argv) {
     long double delta = .001;   // difference in each xy pair
-    long double xStart = -3;    // starting x coordinate
-    long double xEnd = 3;       // enidng x coordinate
-    long double yStart = -3;    // staring y coordinate
-    long double yEnd = 3;       // ending y coordinate
-    int max = 255;         // max rgb digit to be written to the output file
+    long double xStart = 0;     // starting x coordinate
+    long double xEnd = 1;       // enidng x coordinate
+    long double yStart = 0;     // staring y coordinate
+    long double yEnd = 1;       // ending y coordinate
+    int max = 255;              // max rgb digit to be written to the output file
     
-    FILE* output = create_ppm("output.ppm", 6000, 6000, max);  // get file handle for output ppm
+    FILE* output = create_ppm("output.ppm", 1000, 1000, max);  // get file handle for output ppm
     
     // nested loops for moving through xy coordinates
     // moves from max y value to min y value, iterating via delta
